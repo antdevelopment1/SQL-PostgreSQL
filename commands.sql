1891ec1b5c2d69594f4908b47c54bb05a0f014d2
April Copes <antdevelopment1@gmail.com>
2018-11-01 17:45:52 -0400

    How to add multiple WHERE clauses in one SELECT statement

--- commands.sql
+++ commands.sql
@@ -26,3 +26,15 @@ Example:
 SELECT id, name, season FROM products
 WHERE in_stock = 'True';
 
+-- =======================================================
+-- SQL for multiple WHERE clauses in one SELECT statement: 
+-- =======================================================
+
+SELECT column_name1, column_name2 FROM table_name 
+WHERE column_name1 = 'Value'
+AND column_name2 = 'Value';
+Example: 
+
+SELECT id, product_id, quantity FROM orders 
+WHERE product_id = 5
+AND quantity > 1;
