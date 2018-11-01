75234da19ae93b190a263493aff012e9afe7025b
April Copes <antdevelopment1@gmail.com>
2018-11-01 17:44:41 -0400

    How to select all data from a table

---
+++ commands.sql
@@ -0,0 +1,17 @@
+-- ================================
+-- To SELECT all data from a table: 
+-- ================================
+To SELECT all data from a table
+SELECT * FROM table_name; 
+
+Example: 
+SELECT * FROM products; 
+
+
+To SELECT specific columns from a table: 
+SELECT column_name1, column_name2 FROM table_name; 
+
+Example: 
+SELECT first_name, last_name FROM customers; 
+
+
