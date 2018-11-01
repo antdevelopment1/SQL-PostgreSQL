d739b16404f5a81a022bdb3a02714bd775c78049
April Copes <antdevelopment1@gmail.com>
2018-11-01 18:04:04 -0400

    Solves exercies for sql database

---
+++ exercise1.sql
@@ -0,0 +1,14 @@
+-- Exercises 1
+
+SELECT city, postcode FROM address;
+
+SELECT name FROM products
+WHERE season = 'Winter';
+
+SELECT id, quantity, order_date FROM orders
+WHERE price > 25.00
+AND quantity < 4;
+
+SELECT * FROM products
+WHERE season = 'Summer'
+AND wholesale_price >= 20.00;
