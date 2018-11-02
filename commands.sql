048b30382a70ec21bdb4dde537bf2e33a85eeb4e
April Copes <antdevelopment1@gmail.com>
2018-11-01 23:13:33 -0400

    no changes

--- commands.sql
+++ commands.sql
@@ -178,8 +178,8 @@ join products pr on o.product_id = pr.id;
 SELECT t1.column_name1, t1.column_name2, t2.column_name1 FROM table_name1 t1 
 INNER JOIN table_name2 t2 ON t1.column_name1 = t2.column_name2 
 WHERE t1.column_name1 = 'value1';
-Example: 
 
+Example: 
 SELECT p.name, o.quantity, o.order_date FROM products p 
 INNER JOIN orders o ON p.id = o.product_id 
 WHERE p.name = 'Shoes';
