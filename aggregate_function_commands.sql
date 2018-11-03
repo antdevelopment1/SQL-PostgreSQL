d283ec4b246516ab9d26cddfbad31376eba80df3
April Copes <antdevelopment1@gmail.com>
2018-11-03 16:46:17 -0400

    command for min and max values in a given column

--- aggregate_function_commands.sql
+++ aggregate_function_commands.sql
@@ -16,3 +16,21 @@ WHERE column_name = 'value';
 Example: 
 SELECT COUNT(name) FROM products
 WHERE season LIKE '%Summer%';
+
+
+-- ===============================================
+-- SQL FOR RETURNING THE MINIMUM VALUE IN A COLUMN
+-- ===============================================
+SELECT MIN(column_name) FROM table_name; 
+
+Example: 
+SELECT MIN(price) FROM orders; 
+
+
+-- ===============================================
+-- SQL FOR RETURNING THE MAXIMUM VALUE IN A COLUMN
+-- ===============================================
+SELECT MAX(column_name) FROM table_name;  
+
+Example: 
+SELECT MAX(hours) FROM employees; 
